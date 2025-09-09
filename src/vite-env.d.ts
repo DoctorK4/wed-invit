30abfc931b9bae3aef38a6e2e2d470339ca52a1c
doctork <>
2025-09-09 22:03:11 +0900

    feat: add optional VITE_NAVERMAP_API_CID to ImportMetaEnv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-    readonly VITE_APP_SCRIPT_URL: string;
-  }
+  readonly VITE_APP_SCRIPT_URL: string;
+  readonly VITE_NAVERMAP_API_CID?: string;
+}
   
